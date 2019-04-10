0345fa7a12d4f05c60dd7f419fba3d86a2a739d8
Bwitz <guitarrook94@gmail.com>
2019-04-10 12:47:36 -0400

    Added a referral / thanks page

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,32 +1,37 @@
 import React, {Component} from 'react';
+import DefaultHomepage from '../components/DefaultHomepage';
+import ReferralPage from '../components/ReferralPage';
 import '../App.css';
 
 export default class Homepage extends Component {
 
     state={
-        homepage: ""
+        homepage: "default"
+    }
+
+    homepageHandler = () => {
+        switch(this.state.homepage) {
+            case "default":
+                return <DefaultHomepage redirectToReferralHandler={event => this.redirectToReferralHandler()}/>
+            case "referral-page":
+                return <ReferralPage redirectToHomepageHandler={event => this.redirectToHomepageHandler()}/>
+            default:
+                return <DefaultHomepage redirectToReferralHandler={event => this.redirectToReferralHandler()}/>
+        }
+    }
+
+    redirectToReferralHandler = event => {
+        this.setState({homepage: "referral-page"})
+    }
+
+    redirectToHomepageHandler = event => {
+        this.setState({homepage: "default"})
     }
 
 
     render(){
-
         return(
-
-            <div>
-                <h1 className="Homepage text title">Let's talk about it.</h1>
-                <br />
-                <br />
-                <span className="Homepage text span-1">It's understandable that sometimes, life gets tough.</span>
-                <br />
-                <br />
-                <span className="Homepage text span-2"> We believe that it's important to voice out these concerns before they can take a turn for the worse. </span>
-                <br />
-                <br />
-                <span className="Homepage text span-3">Would you like to take a quiz to see if this is a good time to speak with a professional?</span>
-                <br />
-                <button className="Homepage-button">Definitely!</button>
-                <button className="Homepage-button">Not Right Now</button>
-            </div>
+            this.homepageHandler()
         )
     }
 }
